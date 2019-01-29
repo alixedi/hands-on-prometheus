3ca51978d1040ed7335920e53a1f01a6e236411e
Ali Zaidi <alixedi@gmail.com>
2019-01-29 09:33:49 +0000

    Bringing in PID

--- hello/hello/__init__.py
+++ hello/hello/__init__.py
@@ -1,14 +1,15 @@
-from time import sleep
+import os
+from time import time, sleep
 from random import lognormvariate
 
 from flask import Flask, Response, jsonify, request
-from prometheus_client import (
-    Counter, Histogram, Info, make_wsgi_app
-)
+from prometheus_client import Counter, Histogram, Info, make_wsgi_app
 from werkzeug.wsgi import DispatcherMiddleware
 
 
 __version__ = '1.0.0'
+
+PID = os.getpid()
 
 
 def create_app():
@@ -16,11 +17,15 @@ def create_app():
     app = Flask(__name__)
 
     app.request_counter = Counter(
-        'hello_request_count', 'Request throughput', ('name', )
+        'hello_request_count',
+        'Request throughput',
+        ('name', 'pid')
     )
 
     app.request_latency = Histogram(
-        'hello_latency_seconds', 'Request latency',
+        'hello_latency_seconds',
+        'Request latency',
+        ('pid', ),
         buckets=[x * 0.5 for x in range(20)]
     )
 
@@ -28,11 +33,20 @@ def create_app():
         'hello_version', 'Hello version'
     ).info({'version': __version__})
 
+    @app.before_request
+    def _before_request():
+        request._t0 = time()
+
+    @app.after_request
+    def _after_request(response):
+        dt = time() - request._t0
+        app.request_latency.labels(pid=PID).observe(dt)
+        return response
+
     @app.route('/')
     @app.route('/<name>')
-    @app.request_latency.time()
     def hello(name='Stranger'):
-        app.request_counter.labels(name=name).inc()
+        app.request_counter.labels(name=name, pid=PID).inc()
         sleep(lognormvariate(1, 1/4))
         return jsonify(f'Hello {name}!')
 
