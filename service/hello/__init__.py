99b40d0351b918c5354c15ec3e0c909a4d7e6aef
Ali Zaidi <alixedi@gmail.com>
2019-02-03 18:10:26 +0000

    Removing instrumentation

--- service/hello/__init__.py
+++ service/hello/__init__.py
@@ -5,16 +5,9 @@ def create_app():
 
     app = Flask(__name__)
 
-    app.request_counter = Counter(
-        'hello_request_count',
-        'Request throughput',
-        ('name', )
-    )
-
     @app.route('/')
     @app.route('/<name>')
     def hello(name='Stranger'):
-        app.request_counter.labels(name=name).inc()
         return jsonify(f'Hello {name}!')
 
     return app
